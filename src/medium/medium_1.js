de036984ace66a26d954f5f98c030d05051faf58
Ricardo Pojol <ricop@live.unc.edu>
2021-11-03 13:25:20 -0400

    Medium 1 sort fix

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -22,7 +22,7 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    array = array.sort();
+    array.sort((a, b) => a - b);
     console.log(array);
     if(array.length % 2 == 0) {
         return (array[array.length/2] + array[(array.length/2) - 1]) / 2;
@@ -51,7 +51,7 @@ export function getMedian(array) {
  }
  */
 export function getStatistics(array) {
-    array = array.sort();
+    array.sort((a, b) => a-b);
     return {
         'length':array.length,
         'sum':getSum(array),
