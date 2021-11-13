a5a1908c44aead6a90e1e778e0522f71d53eec89
Ricardo Pojol <ricop@live.unc.edu>
2021-11-12 21:28:11 -0500

    currying inital commit

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -44,25 +44,23 @@ export const repeatDemo = () => {
  *   parameter "num2". The returned function should calculate and return the
  *   product of num1 and num2.
  */
-export const multiplyBy = (num1) => {
-
-};
+export const multiplyBy = (num1) => (
+   (num2) => (num1*num2)
+);
 
 
 /**
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
  * Write and export a function named "tenTimesFifty" which uses the tenTimes
  *   function to multiply 50 by 10 and returns the result.
  */
-export const tenTimesFifty = () => {
-
-};
+export const tenTimesFifty = tenTimes(50);
 
 
 /**************************************************************************
--- src/test.js
+++ src/test.js
@@ -2,6 +2,6 @@ import { sumToString, getIncreasingArray, maxAndMin } from "./mild/mild_1.js";
 import { getSum, getMedian} from "./medium/medium_1.js"
 import mpg_data from "./medium/data/mpg_data.js";
 import { searchName } from "./medium/medium_3.js";
-import { repeat, repeatDemo } from "./spicy/spicy_9.js";
+import { multiplyBy, tenTimes, tenTimesFifty } from "./spicy/spicy_9.js";
 
-repeatDemo();
+console.log(tenTimesFifty);
